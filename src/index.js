0b52d0dca0a38409d6320aa9e3ef11d4682b747b
Leo <130.0x82@gmail.com>
2020-04-28 01:20:45 +0200

    switch back to english locale

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import Uppy from '@uppy/core'
 import Dashboard from '@uppy/dashboard'
 import Tus from '@uppy/tus'
-import German from '@uppy/locales/lib/de_DE';
-
 
 require('@uppy/core/dist/style.css')
 require('@uppy/dashboard/dist/style.css')
@@ -10,7 +8,6 @@ require('./index.css')
 
 const uppy = Uppy({
   debug: true,
-  locale: German,
   autoProceed: false,
   restrictions: {
     maxFileSize: 1024 * 1000 * 1000,
@@ -25,7 +22,7 @@ const uppy = Uppy({
   target: '.uppy-dashboard-container',
   replaceTargetContent: true,
   showProgressDetails: true,
-  note: 'Aktuell akzeptieren wir nur Audiodateien.',
+  note: 'We only accept audiofiles.',
   width: "calc(100vw - 20px)",
   height: "calc(100vh - 20px)",
   metaFields: [
