49d898a1bca834908f92ad6c407386922e9b9f33
leonat <130.0x82@gmail.com>
2020-07-22 00:51:44 +0200

    fix shellcheck errors

--- .environment/deploy.sh
+++ .environment/deploy.sh
@@ -8,12 +8,12 @@
 SERVICE_USER=www-data
 
 # remove all files
-sudo rm -rf $DEPLOY_DEST
-sudo mkdir $DEPLOY_DEST
+sudo rm -rf "$DEPLOY_DEST"
+sudo mkdir "$DEPLOY_DEST"
 
 # copy application files
-sudo cp -R $DEPLOY_FROM/dist/. $DEPLOY_DEST
+sudo cp -R "$DEPLOY_FROM"/dist/. "$DEPLOY_DEST"
 
 # chown by user
-sudo chown -R $SERVICE_USER:$SERVICE_USER $DEPLOY_DEST
+sudo chown -R $SERVICE_USER:$SERVICE_USER "$DEPLOY_DEST"
 # restart apache vhost?
